ba12a16b0b49aec30ca1887530469800a24a2c77
sc950828 <1287530097@qq.com>
2020-11-24 11:02:27 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
-git push -f git@github.com:sc950828/sc950828.github.io.git master
+git push -f git@github.com:sc950828/my-blog.git gh-pages
 
 cd -
