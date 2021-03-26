8b31d35c4c19478e742ed7a2d8586b54626adfa1
randy <1287530097@qq.com>
2021-03-26 11:42:20 +0800

    feat: web token

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -1,7 +1,7 @@
 export default function ({ $axios, redirect }) {
   $axios.onRequest((config) => {
     config.headers.Authorization =
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNTY5ZTA1ZmM0MjM1MmUxODAxNjg4MyIsIm5hbWUiOiJyYW5keSIsImlzX2FkbWluIjpmYWxzZSwiaWF0IjoxNjE2MjkzODkxfQ.u5bC4FaifYwxbLOfJY_vyhEMj75t-0htZ9HpBgE0bCw'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNTY5ZTA1ZmM0MjM1MmUxODAxNjg4MyIsIm5hbWUiOiJyYW5keSIsImlzX2FkbWluIjpmYWxzZSwiaWF0IjoxNjE2NzMwMDgyfQ.12u_1ZhMF2KKD1cOw3hJBNiBYE1j37vYn7W_LP3a7Bg'
   })
 
   $axios.onError((error) => {
