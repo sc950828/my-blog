00b395198572343cf9305f9c7b9490efd0fe2255
randy <1287530097@qq.com>
2021-04-16 13:12:16 +0800

    feat: 添加获取请求ip功能

--- server/utils/getIp.js
+++ server/utils/getIp.js
@@ -2,6 +2,7 @@ module.exports = (app) => {
   app.use(async (ctx, next) => {
     console.log(ctx.request.ip);
     console.log(ctx.request.headers['x-forwarded-for']);
+    console.log(ctx.request.ips);
     await next();
   });
 };
